a179b4a662da03d7614964dbea7108f4e3d05341
pedrogaspar72 <gasparpedro72@gmail.com>
2025-09-18 10:50:07 -0300

    instalacao do tailwind e teste de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/djrogerin', function () {
+    echo 'hola';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+});
+
+Route::get('/filmes/{filme}', function ($filme) {
+    echo 'Acessando dados do filme: '.$filme;
+});
+
+Route::view('/conheca', 'sobre');
