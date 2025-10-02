086f5a87af39fb8d6d27054763e6e5fdeffe9aa9
pedrogaspar72 <gasparpedro72@gmail.com>
2025-10-02 10:54:05 -0300

    database

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/djrogerin', function () {
-    echo 'hola';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-});
-
-Route::get('/filmes/{filme}', function ($filme) {
-    echo 'Acessando dados do filme: '.$filme;
-});
-
-Route::view('/conheca', 'sobre');
