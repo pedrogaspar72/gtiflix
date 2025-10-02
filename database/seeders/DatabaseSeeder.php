1d699499cbd7e8820bb9991a0384274ba60108d3
pedrogaspar72 <gasparpedro72@gmail.com>
2025-10-02 10:57:05 -0300

    asdasdas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,17 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    //     User::factory()->create([
+    //         'name' => 'Test User',
+    //         'email' => 'test@example.com',
+    //     ]);
+    // }
+    $this->call([
+        GeneroSeeder::class,
+        AtorSeeder::class
+    ])
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Models\Ator;
+use App\Models\Genero;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/generos', function () {
+    $sexo = Genero::all();
+    dd($sexo);
+});
+
+Route::get('/atores', function () {
+    $ator = Ator::all();
+    dd($ator);
 });
